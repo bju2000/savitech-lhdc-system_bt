4062afe11e9f0c1c1f4a621cbf11274ce615c17d
John Chang <john6636@gmail.com>
2017-08-29 11:05:29 +0800

    Add new codec for LHDC
    Change-Id: I6ef8ffde2cb4b327b1dc63de9de9c2fd94b0fb9d

--- stack/a2dp/a2dp_codec_config.cc
+++ stack/a2dp/a2dp_codec_config.cc
@@ -31,6 +31,7 @@
 #include "a2dp_vendor_aptx.h"
 #include "a2dp_vendor_aptx_hd.h"
 #include "a2dp_vendor_ldac.h"
+#include "a2dp_vendor_lhdc.h"
 #include "osi/include/log.h"
 
 /* The Media Type offset within the codec info byte array */
@@ -119,6 +120,9 @@ A2dpCodecConfig* A2dpCodecConfig::createCodec(
       break;
     case BTAV_A2DP_CODEC_INDEX_SOURCE_LDAC:
       codec_config = new A2dpCodecConfigLdac(codec_priority);
+      break;
+    case BTAV_A2DP_CODEC_INDEX_SOURCE_LHDC:
+      codec_config = new A2dpCodecConfigLhdc(codec_priority);
       break;
     // Add a switch statement for each vendor-specific codec
     case BTAV_A2DP_CODEC_INDEX_MAX:
